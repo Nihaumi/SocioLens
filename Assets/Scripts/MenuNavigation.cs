e72573e550400db00e5e59397009758504f79300
Nathalie Senechal <nathalie.senechal@mailbox.tu-dresden.de>
2023-08-04 14:23:55 +0200

    menu navigation

--- Assets/Scripts/MenuNavigation.cs
+++ Assets/Scripts/MenuNavigation.cs
@@ -1,23 +1,99 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MenuNavigation : MonoBehaviour
 {
-    public void OnButtonTouchBegin()
+
+    //Menu Texts
+    string menuText1 = "SocioLens can recognize the Faces of your coworkers Here is how it works:";
+    string menuText2 = "The Recognition Process will be started when you look at a colleaguegs. ";
+    string menuText3 = "The name, role and time and place of the last encounter for this person will be displayed.";
+    string menuText4 = "Click on the Information to minimize or maximize it again.";
+    string menuText5 = "Have a fun time socializing!";
+    //counter for pagenumbers
+    private int counter = 1;
+    private int numberOfMenuTexts = 5;
+    //TMP
+    [SerializeField] private TMP_Text tmpMenuText;
+    [SerializeField] private TMP_Text pagination;
+    //tutorial canavs
+    [SerializeField] private GameObject tutorialCanvas;
+
+    private void Start()
     {
-        Debug.Log("Begin Touch");
+        ShowTutorial();
+        SetPagination();
     }
-    public void OnButtonTouchEnd()
+
+    private void SetPagination()
+    {
+        pagination.text = counter + "/" + numberOfMenuTexts;
+    }
+
+    public void Previous()
     {
         Debug.Log("End Touch");
+        counter--;
+        ChangeMenuText();
     }
-    public void OnButtonTouchPressed()
+
+    public void Next()
     {
-        Debug.Log("Pressed");
+        Debug.Log("End Touch");
+        counter++;
+        ChangeMenuText();
     }
-    public void OnButtonTouchReleased()
+
+    public void EndTutorial()
     {
-        Debug.Log("Button Released");
+        counter = 1;
+        SetPagination();
+        tutorialCanvas.SetActive(false);
     }
+
+    public void ShowTutorial()
+    {
+        counter = 1;
+        SetPagination();
+        ChangeMenuText();
+        tutorialCanvas.SetActive(true);
+    }
+
+    private void ChangeMenuText()
+    {
+        if(counter < 1)
+        {
+            counter = 1;
+        }
+        if(counter > numberOfMenuTexts)
+        {
+            counter = numberOfMenuTexts;
+        }
+        switch (counter)
+        {
+            case 1:
+                tmpMenuText.text = menuText1;
+                break;   
+            case 2:
+                tmpMenuText.text = menuText2;
+                break;
+            case 3:
+                tmpMenuText.text = menuText3;
+                break;
+            case 4:
+                tmpMenuText.text = menuText4;
+                break;
+            case 5:
+                tmpMenuText.text = menuText5;
+                break;
+            default: 
+                break;
+
+        }
+        SetPagination();
+    }
+
+
 }
