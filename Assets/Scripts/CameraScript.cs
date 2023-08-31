e2bb0e4e376c1d7841a442bc01be3d9ede60f5f8
LAPTOP-TFQVDDGS\inuch <nathalie.senechal@mailbox.tu-dresden.de>
2023-08-31 11:21:12 +0200

    add nuget and websocketsharp, added webcam recognition with server

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using WebSocketSharp;
+
+namespace csharp_client
+{
+
+    public class CameraScript : MonoBehaviour
+    {
+        //TODO use hololens cam instead of webcam
+        public float framecounter = 0.0f;
+        public Text startStopText;
+        public Text cam;
+        int currentCamIndex = 0;
+        WebCamTexture tex;
+        public RawImage display;
+        WebSocket ws;
+        public byte[] mybytes;
+
+        public void SwapCam_Clicked()
+        {
+
+            if (WebCamTexture.devices.Length > 0)
+            {
+                currentCamIndex += 1;
+                currentCamIndex %= WebCamTexture.devices.Length;
+
+                if (startStopText != null)
+                {
+                    StopWebcam();
+                    StartStopCam_Clicked();
+                }
+            }
+        }
+
+        public async void StartStopCam_Clicked()
+        {
+            if (tex != null) // Stop the camera
+            {
+                StopWebcam();
+                startStopText.text = "Start Camera";
+
+            }
+            else    // Start the camera
+            {
+                WebCamDevice device = WebCamTexture.devices[currentCamIndex];
+                tex = new WebCamTexture(device.name);
+                display.texture = tex;
+                cam.text = device.name;
+                tex.Play();
+                startStopText.text = "Stop Camera";
+
+                //open Websocket-Connection
+                ws = new WebSocket("ws://127.0.0.1:8000/ws");
+                ws.OnMessage += (sender, e) => {
+                    if (e.IsText)
+                    {
+                        Debug.Log("SERVER SAYS: " + e.Data);
+                    }
+                    else
+                    {
+                        Debug.Log("NO TEXT RECEIVED");
+                    }
+                };
+
+                ws.Connect();
+
+            }
+        }
+
+        private void StopWebcam()
+        {
+            // close Websocket-connection
+            //ws.Send("close");
+            ws.Close();
+
+            display.texture = null;
+            tex.Stop();
+            tex = null;
+        }
+        void Start()
+        {
+
+            WebCamDevice[] devices = WebCamTexture.devices;
+            for (int i = 0; i < devices.Length; i++)
+            {
+
+                Debug.Log(devices[i].name);
+            }
+        }
+
+        async void Update()
+        {
+
+            framecounter += Time.deltaTime;
+
+            if (framecounter >= 2 && tex != null)
+            {
+                Texture2D snap = new Texture2D(tex.width, tex.height);
+                snap.SetPixels(tex.GetPixels());
+                snap.Apply();
+
+                mybytes = snap.EncodeToJPG();
+
+                // SEND IMAGE VIA WEBSOCKET TO SERVER
+                ws.Send(mybytes);
+                framecounter = 0;
+            }
+
+        }
+
+
+    }
+}
+
+
