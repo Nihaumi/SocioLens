74f9803a639cf91523bf2e7854da95b310470b2a
Nathalie Senechal <nathalie.senechal@mailbox.tu-dresden.de>
2023-09-06 10:16:21 +0200

    delete unnecessary camera part

--- Assets/Scripts/Camera/CameraScript.cs
+++ Assets/Scripts/Camera/CameraScript.cs
@@ -31,63 +31,7 @@ namespace csharp_client
         private PhotoCapture photoCaptureObject = null;
 
         //hololens Cam tutorial
-#if !UNITY_EDITOR
-        #region
-        public void Start()
-        {
-            PhotoCapture.CreateAsync(OnPhotoCaptureCreated);
-        }
 
-        private void OnPhotoCaptureCreated(PhotoCapture captureObject)
-        {
-            photoCaptureObject = captureObject;
-
-            Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
-
-            CameraParameters c = new CameraParameters();
-            c.hologramOpacity = 0.0f;
-            c.cameraResolutionWidth = cameraResolution.width;
-            c.cameraResolutionHeight = cameraResolution.height;
-            c.pixelFormat = CapturePixelFormat.BGRA32;
-
-            captureObject.StartPhotoModeAsync(c, OnPhotoModeStarted);
-        }
-
-        private object OnPhotoModeStarted(PhotoCapture.PhotoCaptureResult result)
-        {
-            if (result.success)
-            {
-                string filename = string.Format(@"CapturedImage{0}_n.jpg", Time.time);
-                string filePath = System.IO.Path.Combine(Application.persistentDataPath, filename);
-
-                photoCaptureObject.TakePhotoAsync(filePath, PhotoCaptureFileOutputFormat.JPG, OnCapturedPhotoToDisk);
-            }
-            else
-            {
-                Debug.LogError("Unable to start photo mode!");
-            }
-        }
-        void OnCapturedPhotoToDisk(PhotoCapture.PhotoCaptureResult result)
-        {
-            if (result.success)
-            {
-                Debug.Log("Saved Photo to disk!");
-                photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
-            }
-            else
-            {
-                Debug.Log("Failed to save Photo to disk");
-            }
-        }
-
-        void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
-        {
-            photoCaptureObject.Dispose();
-            photoCaptureObject = null;
-        }
-
-        #endregion
-#endif
         public void SwapCam_Clicked()
         {
 
